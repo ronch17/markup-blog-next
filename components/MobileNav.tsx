169cea627d783f6ccff49197a49fe77a7c90607f
Ron Dayan <rondayan@mac.local>
2025-07-05 23:01:33 +0300

    Blog Page with posts added

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,70 +1,82 @@
-'use client'
+"use client";
 
-import {useState} from "react";
-import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
-import {Button} from "@/components/ui/button";
-import {Menu} from "lucide-react";
-import {useRouter} from "next/navigation";
-import Link, {LinkProps} from "next/link";
+import { useState } from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { useRouter } from "next/navigation";
+import Link, { LinkProps } from "next/link";
 import { Icons } from "./icons";
-import {siteConfig} from "@/config/site";
+import { siteConfig } from "@/config/site";
 
 export function MobileNav() {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
-    return (
-        <Sheet  open={open} onOpenChange={setOpen}>
-            <SheetTrigger asChild>
-                <Button variant="outline" className="w-10 px-0 sm:hidden">
-                    <Menu className="h-5 w-5" />
-                    <span className="sr-only">Toggle Theme</span>
-                </Button>
-            </SheetTrigger>
-            <SheetContent side="right">
-                <MobileLink onOpenChange={setOpen} href="/" className="flex items-center pt-3 px-4">
-                    <Icons.logo className="mr-2 h-4 w-4" />
-                    <span className="font-bold">{siteConfig.name}</span>
-                </MobileLink>
-                <div className="flex flex-col gap-3 mt-3 px-4">
-                    <MobileLink onOpenChange={setOpen} href="/blog">
-                        Blog
-                    </MobileLink>
-                    <MobileLink onOpenChange={setOpen} href="/about">
-                        About
-                    </MobileLink>
-                    <Link target="_blank" rel="noreferrer" href={siteConfig.links.github}>GitHub</Link>
-                    <Link target="_blank" rel="noreferrer" href={siteConfig.links.personalSite}>Personal Website</Link>
-                </div>
-            </SheetContent>
-        </Sheet>
-    )
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button variant="outline" className="w-10 px-0 sm:hidden">
+          <Menu className="h-5 w-5" />
+          <span className="sr-only">Toggle Theme</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="right">
+        <MobileLink
+          onOpenChange={setOpen}
+          href="/"
+          className="flex items-center pt-3 px-4"
+        >
+          <Icons.logo className="mr-2 h-4 w-4" />
+          <span className="font-bold">{siteConfig.name}</span>
+        </MobileLink>
+        <div className="flex flex-col gap-3 mt-3 px-4">
+          <MobileLink onOpenChange={setOpen} href="/blog">
+            Blog
+          </MobileLink>
+          <MobileLink onOpenChange={setOpen} href="/about">
+            About
+          </MobileLink>
+          <Link target="_blank" rel="noreferrer" href={siteConfig.links.github}>
+            GitHub
+          </Link>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={siteConfig.links.personalSite}
+          >
+            Personal Website
+          </Link>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 }
 
 interface MobileLinkProps extends LinkProps {
-    children: React.ReactNode;
-    onOpenChange: (open: boolean) => void;
-    className?: string;
+  children: React.ReactNode;
+  onOpenChange: (open: boolean) => void;
+  className?: string;
 }
 
 function MobileLink({
-    href,
-    onOpenChange,
-    children,
-    className,
-    ...props
+  href,
+  onOpenChange,
+  children,
+  className,
+  ...props
 }: MobileLinkProps) {
-        const router = useRouter();
-        return (
-            <Link
-                href={href}
-                onClick={() => {
-            router.push(href.toString());
-            onOpenChange?.(false);
-        }}
-                className={className}
-            {...props}
-            >
-            {children}
-        </Link>
-);
+  const router = useRouter();
+  return (
+    <Link
+      href={href}
+      onClick={() => {
+        router.push(href.toString());
+        onOpenChange?.(false);
+      }}
+      className={className}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
 }
--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -1,35 +1,54 @@
 import Link from "next/link";
-import {cn} from "@/lib/utils";
-import {siteConfig} from "@/config/site";
-import {buttonVariants} from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { siteConfig } from "@/config/site";
+import { buttonVariants } from "@/components/ui/button";
 import { Icons } from "./icons";
-import {MainNav} from "@/components/mainNav";
-import {MobileNav} from "@/components/MobileNav";
-import {ModeToggle} from "@/components/ModeToggle";
+import { MainNav } from "@/components/mainNav";
+import { MobileNav } from "@/components/MobileNav";
+import { ModeToggle } from "@/components/ModeToggle";
 
 export function SiteHeader() {
-    return <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur support-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-14 max-w-screen-2xl items-center px-4">
-            <MainNav />
-            <div className="flex flex-1 items-cetner justify-end space-x-2">
-                <nav className="flex items-center">
-                <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
-                    <div className={cn(buttonVariants({variant: 'ghost'}), "w-10 px-0 hidden sm:inline-flex")}>
-                        <Icons.gitHub className="h-4 w-4" />
-                        <span className="sr-only">Github</span>
-                    </div>
-                </Link>
-                <Link href={siteConfig.links.personalSite} target="_blank" rel="noreferrer">
-                    <div className={cn(buttonVariants({variant: 'ghost'}), "w-10 px-0 hidden sm:inline-flex")}>
-                        <Icons.logo className="h-4 w-4" />
-                        <span className="sr-only">Personal Site</span>
-                    </div>
-                </Link>
-                    <ModeToggle />
-                <MobileNav />
-                </nav>
-
-            </div>
+  return (
+    <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur support-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center px-4">
+        <MainNav />
+        <div className="flex flex-1 items-cetner justify-end space-x-2">
+          <nav className="flex items-center">
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={cn(
+                  buttonVariants({ variant: "ghost" }),
+                  "w-10 px-0 hidden sm:inline-flex",
+                )}
+              >
+                <Icons.gitHub className="h-4 w-4" />
+                <span className="sr-only">Github</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.personalSite}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={cn(
+                  buttonVariants({ variant: "ghost" }),
+                  "w-10 px-0 hidden sm:inline-flex",
+                )}
+              >
+                <Icons.logo className="h-4 w-4" />
+                <span className="sr-only">Personal Site</span>
+              </div>
+            </Link>
+            <ModeToggle />
+            <MobileNav />
+          </nav>
         </div>
+      </div>
     </header>
+  );
 }
